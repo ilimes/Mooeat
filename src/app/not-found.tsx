2003540c3514143f3d825fea6537ec9a276a1022
LIME <95404736+ilimes@users.noreply.github.com>
2023-12-03 22:15:48 +0900

    font size, not-fonund 변경

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -13,7 +13,7 @@ const NotFound = () => {
       <Col xs={24} sm={24} md={24} lg={24} xl={10} xxl={10} style={{ textAlign: 'center' }}>
         <Image src={NotFoundImage} alt='not found' width={250} style={{ marginTop: 20 }} />
         <h2>앗! 죄송해요.</h2>
-        <div style={{ marginBottom: 30, color: '#00000078', fontSize: 15 }}>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 2, marginBottom: 30, color: '#00000078', fontSize: 14 }}>
           <div>요청하신 페이지를 찾을 수 없어요.</div>
           <div>찾으시려는 페이지의 주소가 잘못 입력되었거나,</div>
           <div>페이지 주소가 변경 또는 삭제되어</div>
