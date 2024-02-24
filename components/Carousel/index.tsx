d9f0d9abfe72a29fbf29b696b7bbb3bc662a3133
LIME <95404736+ilimes@users.noreply.github.com>
2024-02-24 16:38:48 +0900

    test

--- components/Carousel/index.tsx
+++ components/Carousel/index.tsx
@@ -25,7 +25,7 @@ const CarouselComponent = () => {
   const imgHeight = isMobile ? 200 : 350;
   const contents = [
     {
-      topText: 'Mooeat에 오신것을 환영합니다!',
+      topText: 'Mooeat에 오신것을 환영합니다!1',
       bottomText: 'Mooeat 사이트가 오픈하였습니다 🎉',
       forwardText: '자세히보기',
       background: '#47408f',
