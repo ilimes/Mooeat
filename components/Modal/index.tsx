cf70b4877c6fa24bf2b58d2bad494a6d7463ea87
ilimes <ilimes1307@gmail.com>
2024-07-13 09:44:27 +0900

    Modal 애니메이션 수정

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Modal } from 'antd';
 import { ModalPropsTypes } from '@/types/Common/Common.interface';
+import './animation-style.css';
 
 const ModalComponent = ({ children, title, isOpen, closeModal }: ModalPropsTypes) => (
-  <Modal title={title} open={isOpen} onCancel={closeModal} footer={false} centered destroyOnClose>
+  <Modal
+    title={title}
+    open={isOpen}
+    onCancel={closeModal}
+    footer={false}
+    centered
+    transitionName="my-modal"
+    destroyOnClose
+  >
     <div style={{ marginTop: 20 }}>{children}</div>
   </Modal>
 );
