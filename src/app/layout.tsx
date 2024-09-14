28eeb337941fe0e2adae3b6edafcb403a7599680
LIME <95404736+ilimes@users.noreply.github.com>
2024-09-14 16:32:20 +0900

    splash image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,44 +26,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <meta name="apple-mobile-web-app-capable" content="yes" />
         <link rel="apple-touch-icon" sizes="192x192" href="maskable_icon_x192.png" />
         <link rel="apple-touch-icon" sizes="512x512" href="maskable_icon_x512.png" />
-        <link
-          href="/splash_screens/12.9__iPad_Pro_landscape.png"
-          sizes="2048x2732"
-          rel="apple-touch-startup-image"
-        />
-        <link
-          href="/splash_screens/10.9__iPad_Air_portrait.png"
-          sizes="1668x2224"
-          rel="apple-touch-startup-image"
-        />
-        <link
-          href="/splash_screens/10.5__iPad_Air_portrait.png"
-          sizes="1536x2048"
-          rel="apple-touch-startup-image"
-        />
-        <link
-          href="/splash_screens/iPhone_11_Pro_Max__iPhone_XS_Max_portrait.png"
-          sizes="1125x2436"
-          rel="apple-touch-startup-image"
-        />
-        <link
-          href="/splash_screens/iPhone_8_Plus__iPhone_7_Plus__iPhone_6s_Plus__iPhone_6_Plus_portrait.png"
-          sizes="1242x2208"
-          rel="apple-touch-startup-image"
-        />
-        <link
-          href="/splash_screens/iPhone_8__iPhone_7__iPhone_6s__iPhone_6__4.7__iPhone_SE_portrait.png"
-          sizes="750x1334"
-          rel="apple-touch-startup-image"
-        />
-        <link
-          href="/splash_screens/4__iPhone_SE__iPod_touch_5th_generation_and_later_portrait.png"
-          sizes="640x1136"
-          rel="apple-touch-startup-image"
-        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <link href="/apple_splash_2048.png" sizes="2048x2732" rel="apple-touch-startup-image" />
+        <link href="/apple_splash_1668.png" sizes="1668x2224" rel="apple-touch-startup-image" />
+        <link href="/apple_splash_1536.png" sizes="1536x2048" rel="apple-touch-startup-image" />
+        <link href="/apple_splash_1125.png" sizes="1125x2436" rel="apple-touch-startup-image" />
+        <link href="/apple_splash_1242.png" sizes="1242x2208" rel="apple-touch-startup-image" />
+        <link href="/apple_splash_750.png" sizes="750x1334" rel="apple-touch-startup-image" />
+        <link href="/apple_splash_640.png" sizes="640x1136" rel="apple-touch-startup-image" />
       </head>
       <body>
         <main>
