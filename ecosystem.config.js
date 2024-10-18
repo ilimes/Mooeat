6220d8d08b1ff67c1c6fb88fd6052118606bad9b
ilimes <ilimes1307@gmail.com>
2024-10-18 14:31:54 +0900

    deploy 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,18 @@
-// ./ecosystem.config.js
 module.exports = {
-    apps: [
-      {
-        name: "mooeat",
-        script: "node_modules/next/dist/bin/next",
-        args: "start --port 3000",
-        instances: 1,
-        exec_mode: "cluster",
-        watch: true, // 파일 변경 모니터링, 파일 변동시 재시작
+  apps: [
+    {
+      name: 'mooeat',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start --port 3000',
+      instances: process.env.NODE_ENV === 'production' ? 'max' : 1,
+      exec_mode: process.env.NODE_ENV === 'production' ? 'cluster' : 'fork',
+      watch: process.env.NODE_ENV !== 'production',
+      env: {
+        NODE_ENV: 'development',
       },
-    ],
-  };
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 
+const prod = process.env.NODE_ENV === 'production'
+
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
+  disable: prod ? false : true,
   skipWaiting: true,
 });
 
